7285332df97c72d54416785bc168cc9920f651f6
yann300 <yann.levreau@gmail.com>
2020-12-03 00:34:13 +0100

    add ipfs node

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const remixd = require('remixd')
 const path = require('path')
 const os = require('os')
+const IPFS = require('ipfs')
+const IPFSGateway = require('ipfs-http-gateway')
 
 const { version } = require('./package.json')
 const applicationMenu = require('./applicationMenu')
@@ -33,11 +35,34 @@ function createWindow () {
     shell.openExternal(url);
   })
   win.loadURL('package://github.com/ethereum/remix-project')
+
+  // Modify the user agent for all requests to the following urls.
+  const filter = {
+    urls: ['https://*.dyn.plugin.remixproject.org/ipfs/*']
+  }
+  let hashes = {}
+  win.webContents.session.webRequest.onBeforeRequest(filter, (details, callback) => {
+    let { url } = details;
+    var reg = /dyn.plugin.remixproject.org\/ipfs\/(.*)/
+    var regResult = reg.exec(url)
+    const hash = regResult[1]
+    hashes[hash] = url
+    url = `http://localhost:5001/ipfs/${regResult[1]}`      
+    callback({
+      cancel: false,
+      url: ( encodeURI(url ) )
+    })
+  })  
+    
+  win.webContents.session.webRequest.onErrorOccurred((details) => {
+    console.error(details)
+  })
 }
 
 app.on('ready', () => {
   remixdStart()
-  createWindow()  
+  createWindow()
+  ipfsStart()
 })
 
 let remixdStart = () => {
@@ -52,5 +77,26 @@ let remixdStart = () => {
     })
   } catch (error) {
     throw new Error(error)
+  }
+}
+
+let ipfsStart = async () => {
+  try {
+    const repo = os.homedir() + '/.remix-ipfsnode'
+    const node = await IPFS.create({
+      repo,
+      config: {
+        Addresses: {
+          Gateway: `/ip4/127.0.0.1/tcp/5001`
+        }        
+      }
+    })
+    const gateway = new IPFSGateway(node)
+    const id = await node.id()
+    gateway.start()
+    console.log('ipfs node', id)
+    console.log('ipfs node repo', repo)
+  } catch (err) {
+    console.error(err)
   }
 }
